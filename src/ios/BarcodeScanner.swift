75567fa3b7415595ebfa5cf043e1e6cf4bc802e2
Joachim Klahr <joachim.klahr@gmail.com>
2020-10-07 16:23:59 +0200

    Added support for iOS.

--- src/ios/BarcodeScanner.swift
+++ src/ios/BarcodeScanner.swift
@@ -1,5 +1,10 @@
-class BarcodeScanner: NSObject {
+import AVFoundation
+import UIKit
+
+class BarcodeScanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 	private var listeners: Set<Int64> = Set()
+	private var captureSession: AVCaptureSession!
+	private var previewLayer: AVCaptureVideoPreviewLayer!
 
 	func addListener(listener: Int64) {
 		listeners.insert(listener)
@@ -14,7 +19,87 @@ class BarcodeScanner: NSObject {
 	}
 
 	func scan() {
-		// TODO: Implement magic.
+		captureSession = AVCaptureSession()
+		guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+			return
+		}
+		let videoInput: AVCaptureDeviceInput
+
+		do {
+			videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+		} catch {
+			return
+		}
+
+		if (captureSession.canAddInput(videoInput)) {
+			captureSession.addInput(videoInput)
+		} else {
+			return
+		}
+
+		let metadataOutput = AVCaptureMetadataOutput()
+
+		if (captureSession.canAddOutput(metadataOutput)) {
+			captureSession.addOutput(metadataOutput)
+
+			metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+			metadataOutput.metadataObjectTypes = [.qr]
+		} else {
+			return
+		}
+		
+		previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+		previewLayer.frame = view.layer.bounds
+		previewLayer.videoGravity = .resizeAspectFill
+		view.layer.addSublayer(previewLayer)
+
+		let appDelegate = UIApplication.shared.delegate as! AppDelegate
+		appDelegate.window?.rootViewController?.present(self, animated: true)
+		
+		captureSession.startRunning()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		if (captureSession?.isRunning == false) {
+			captureSession.startRunning()
+		}
+	}
+
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+
+		if (captureSession?.isRunning == true) {
+			captureSession.stopRunning()
+		}
+	}
+
+	func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+		captureSession.stopRunning()
+
+		if let metadataObject = metadataObjects.first {
+			guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+			guard let stringValue = readableObject.stringValue else { return }
+			AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+			found(code: stringValue)
+		}
+
+		dismiss(animated: true)
+	}
+	
+	func found(code: String) {
+		listeners.forEach { listener in
+			BarcodeScannerDelegate.onBarcodeScanResult(listener, code: code)
+		}
+	}
+	
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+
+	override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+		return .portrait
 	}
 }
 
